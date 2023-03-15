b224cb8a6bbc2d38566f577c6310b6a517dc3035
Clare <110568186+clarefcampbell@users.noreply.github.com>
2023-03-16 02:26:06 +1300

    Added note for improving solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -11,5 +11,7 @@ const fibonacci = function(member) {
     return fib[member]
 };
 
+//Better solution would be to use temp variables rather than growing an array. Would be faster and less memory. 
+
 // Do not edit below this line
 module.exports = fibonacci;
