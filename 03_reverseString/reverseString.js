e994ec43886888f5d23641557a7536884f6c2f47
Clare <110568186+clarefcampbell@users.noreply.github.com>
2023-03-15 23:14:11 +1300

    Complete exercise 1-3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    const strArray = str.split('');
+    return strArray.reverse().join('');
 };
 
 // Do not edit below this line
