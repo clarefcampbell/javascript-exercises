c23abb53304ca3dd2dbdf12936f4af4920021062
Clare <110568186+clarefcampbell@users.noreply.github.com>
2023-03-16 01:19:42 +1300

    Complete exercises 4-7

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(origArray, ...valToRemove) {
+    for (let i = 0 ; i < origArray.length ; i++) {
+        for (let j = 0; j < valToRemove.length ; j++) {
+            if (origArray[i] === valToRemove[j]) {
+                origArray[i] = null;
+            }
+        }
+    }
+    return origArray.filter(Boolean);
+    
 };
+
+//Alternative solution would be to take advantage of the includes() method, which I learned about from the solution.
 
 // Do not edit below this line
 module.exports = removeFromArray;
