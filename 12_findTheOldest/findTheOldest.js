47abf10929e7d81e9ffe33a40b846bc9dc5345af
Clare <110568186+clarefcampbell@users.noreply.github.com>
2023-03-17 03:45:47 +1300

    Complete exercises 11-13

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce(
+        (currentOldest, nextPerson) => 
+                getAge(currentOldest) > getAge(nextPerson) ?
+                currentOldest : nextPerson
+        ,people[0]
+    );
 };
+
+function getAge(person) {
+    return person.hasOwnProperty("yearOfDeath") ?
+            person.yearOfDeath - person.yearOfBirth :
+            new Date().getFullYear() - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,25 @@
-const caesar = function() {
+const caesar = function(original, shift) {
 
+    //Handle large shifts
+    while (shift > 26 || shift < -26) {
+        shift = (shift < 0) ? shift + 26 : shift - 26;
+    }
+
+    return original.split('').map((letter) => {
+        //Converts if letter to ascii
+        if (letter.charCodeAt() <= 90 && letter.charCodeAt() >= 65 || (letter.charCodeAt() >= 97 && letter.charCodeAt() <= 122)) {
+            letter = letter.charCodeAt();
+        }
+        //Shift and correct if outside range
+        if (typeof(letter) == 'number' && shift > 0)  {
+            letter = ((letter <= 90 && letter + shift > 90) || (letter + shift > 122)) ? letter + shift - 26 : letter + shift;
+        } else if (typeof(letter) == 'number' && shift < 0) {
+            letter = ((letter >= 97 && letter + shift < 97) || (letter + shift < 65)) ? letter + shift + 26 : letter + shift;
+        }
+        //Back to string
+        return (typeof(letter) == 'number') ? String.fromCharCode(letter) : letter;
+    //Array to String
+    }).join('');
 };
 
 // Do not edit below this line
